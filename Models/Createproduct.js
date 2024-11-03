04a694c036ccdef91ffc45d57357b608708f9ac5
thameemansari <thameemrpp@gmail.com>
2024-11-03 14:07:28 +0530

    gg

--- Models/Createproduct.js
+++ Models/Createproduct.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose');
 const CreateproductSchema =  new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        
     },
     price: {
         type: Number,
-        required: true
+        
     },
     description: {
         type: String,
-        required: true
+        
     },
     mineImage: {
         type: String,
-        required: true
+        
     },
     subImage: {
         type: String,
-        required: true
+        
     },
     category: {
         type: String,
-        required: true
+        
     }
 });
 
