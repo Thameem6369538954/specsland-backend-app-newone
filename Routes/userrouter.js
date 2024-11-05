86f46c440604a1cca3e4b23aa30995dcdb1a7f8b
thameemansari <thameemrpp@gmail.com>
2024-11-05 10:21:35 +0530

    register sun

--- Routes/userrouter.js
+++ Routes/userrouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser } = require("../controllers/usercontroller");
+const { createUser, loginUser, getUser } = require("../controllers/usercontroller");
 
 
 router.route("/userRegister").post(createUser);
-router.route("/userRegister").get(createUser);
+router.route("/getUser").get(getUser);
 router.route("/login").post(loginUser);
 
 module.exports = router
