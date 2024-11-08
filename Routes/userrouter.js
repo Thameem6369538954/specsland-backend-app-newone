84a4f5894d5bfbfa594b7051b7be48dd5c4359a2
thameemansari <thameemrpp@gmail.com>
2024-11-08 18:53:58 +0530

    logout

--- Routes/userrouter.js
+++ Routes/userrouter.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser, getUser } = require("../controllers/usercontroller");
+const {
+  createUser,
+  loginUser,
+  getUser,
+  userlogout,
+} = require("../controllers/usercontroller");
 
 
 router.route("/userRegister").post(createUser);
 router.route("/getUser").get(getUser);
 router.route("/login").post(loginUser);
+router.route("/logout").post(userlogout);
 
 module.exports = router
--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -113,6 +113,14 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-
+exports.userlogout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    console.error("Error during logout:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
 
 
