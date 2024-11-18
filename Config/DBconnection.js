9e592de34aca43cfa9a0997050ffc3d1dd6b7338
thameemansari <thameemrpp@gmail.com>
2024-11-18 20:45:06 +0530

    second hello

--- Config/DBconnection.js
+++ Config/DBconnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () =>{
     try {
-        const conn = await mongoose.connect(process.env.DB_LOCAL_URL);
+        const conn = await mongoose.connect(process.env.DB_LOCAL_URL) || "mongodb+srv://thameemrpp:thameemrpp@specsland.dz8d2.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Specsland";
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
     } catch (error) {
